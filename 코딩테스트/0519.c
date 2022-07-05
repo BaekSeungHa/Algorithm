1124af59ee642a95ccc59bae1074c5c24af0a83e
BaekSeungHa <101560680+BaekSeungHa@users.noreply.github.com>
2022-07-05 20:41:51 +0900

    Create 0519.c

---
+++ 코딩테스트/0519.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+ 
+int main() {
+	int n;
+	scanf("%d", &n);
+	if(n%10 != 0) {
+		printf("-1");
+	}
+	else {
+		printf("%d", n/300);
+		n%=300;
+		printf("%d", n/60);
+		n%=60;
+		printf("%d", n/10);
+	}
+}
